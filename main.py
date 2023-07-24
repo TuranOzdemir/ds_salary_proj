086950dc3ef207bc3d7a04dea54185c2ccd50cf1
Turan ÖZDEMİR <62646952+TuranOzdemir@users.noreply.github.com>
2023-07-24 14:49:07 +0300

    Update main.py
    add some comment about verbose

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ import to_excel as ex
 keyword = "data scientist"
 num_jobs = 3
 df = gs.get_jobs(keyword, num_jobs, verbose=False, verbose2=False, slp_time=5)
+# verboses are for testing (can we collect the data properly or not, it prints it on the screen while collecting)
+# If you're not testing, leave it as is.
